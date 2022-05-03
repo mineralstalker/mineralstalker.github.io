c9a349a34f9969b23e022611c5ae8717351b8bf1
mineralstalker <104847447+mineralstalker@users.noreply.github.com>
2022-05-03 09:28:58 -0600

    Add files via upload

---
+++ data/Concentration_Data/Sampler_Concentraions/Concentrations.geojson.js
@@ -0,0 +1,25 @@
+var samplecons = {
+  type: "FeatureCollection",
+  generator: "Field Geo Services",
+  copyright: "Data provided by Field Geo Services.",
+  timestamp: "2022-2-18",
+  features: [
+    {
+      type: "Feature",
+      properties: {
+        1950: 331314,
+        1960: 487455,
+        1970: 496973,
+        1980: 425022,
+        1990: 394017,
+        2000: 416474,
+        2010: 331314,
+        name: "Sampler 1",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [-107.88583, 39.53907],
+      },
+    },
+  ],
+};
---
+++ data/hydrocarbon.geojson.js
@@ -0,0 +1,71 @@
+var hydrocarbon_wells = {
+    "type": "FeatureCollection",
+    "generator": "overpass-turbo",
+    "copyright": "The data included in this document is from www.openstreetmap.org. The data is made available under ODbL.",
+    "timestamp": "2015-08-08T19:02:02Z",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "variety": "Oil Well",
+          "status": "Active Production",
+          "operator": "TEP ROCKY MOUNTAIN LLC",
+          "name": "110 CLOUGH",
+          "location": "39.527868, -107.89009",
+          "date_spud": "Unknown",
+          "date_shut_in": "N/A",
+          "casing_depths": '<a href="data/Sources/Wells/Casing_Depths.png" target="_blank" rel="noopener noreferrer"> <button> Depth Diagrams </button></a>',
+          "leak_reports": '<a href="Internal_Functions/csv_uploader.html" target="_blank" rel="noopener noreferrer"> <button> Reports </button></a>',
+          "other": "N/A"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -107.89009348311355, 39.5278685700271
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "variety": "Oil Well",
+          "status": "Active Production",
+          "operator": "TEP ROCKY MOUNTAIN LLC",
+          "name": "RWF 511-9 FEDERAL",
+          "location": "39.53429, -107.90074",
+          "date_spud": "05-30-2007",
+          "date_shut_in": "N/A",
+          "casing_depths": '<a href="data/Sources/Wells/Casing_Depths.png" target="_blank" rel="noopener noreferrer"> <button> Depth Diagrams </button></a>',
+          "leak_reports": '<a href="Internal_Functions/csv_uploader.html" target="_blank" rel="noopener noreferrer"> <button> Reports </button></a>',
+          "other": "N/A"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -107.90074887873085, 39.53429368451655
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "variety": "Oil Well",
+          "status": "Active Production",
+          "operator": "TEP ROCKY MOUNTAIN LLC",
+          "name": "NR 422-3 Clough",
+          "location": "39.55223, -107.87702",
+          "date_spud": "10-14-2011",
+          "date_shut_in": "N/A",
+          "casing_depths": '<a href="data/Sources/Wells/Casing_Depths.png" target="_blank" rel="noopener noreferrer"> <button> Depth Diagrams </button></a>',
+          "leak_reports": '<a href="Internal_Functions/csv_uploader.html" target="_blank" rel="noopener noreferrer"> <button> Reports </button></a>',
+          "other": "N/A"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -107.87702642683539, 39.552237162942156
+          ]
+        }
+      }
+    ]
+  };
