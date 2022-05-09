ce30d10d736e15ca232bae7d4b41d6ae8f72803a
airguardianfgs <airguardian@fieldgeoservices.comm>
2022-05-09 07:40:23 -0600

    commit

---
+++ data/publicsites.geojson.js
@@ -0,0 +1,76 @@
+var publicsites = {
+  type: "FeatureCollection",
+  generator: "Field Geo Services",
+  copyright:
+    "Data included in this document is from ESRI. The data is made available under ODbL.",
+  timestamp: "2022-1-12",
+  features: [
+    {
+      type: "Feature",
+      id: "tbd",
+      properties: {
+        "@id": "tbd",
+        date_online: "1/1/2021",
+        date_offline: "N/A",
+        territory: "Garfield County, CO",
+        amenity: "Sampler",
+        tempatures_gra:
+          '<a href="http://air.terrasls.com:3000/AirMonitor/d/KoGnWF27k/devon-combined-view?orgId=2&viewPanel=16" target="_blank" rel="noopener noreferrer"> <button> Tempature </button></a>',
+        humity_gra:
+          '<a href="http://air.terrasls.com:3000/AirMonitor/d/KoGnWF27k/devon-combined-view?orgId=2&viewPanel=17" target="_blank" rel="noopener noreferrer"> <button> &nbsp;&nbsp;Humidity&nbsp; </button></a>',
+        grafana:
+          '<a href="http://air.terrasls.com:3000/AirMonitor/d/KoGnWF27k/devon-combined-view?orgId=2" target="_blank" rel="noopener noreferrer"> <button> &nbsp;VOC&nbsp; </button></a>',
+        graphs:
+          '<a href="Internal_Functions/csv_uploader.html" target="_blank" rel="noopener noreferrer"> <button> &nbsp;CSV&nbsp; </button></a>',
+        name: "Sampler 1",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [-107.88501, 39.53999],
+      },
+    },
+    {
+      type: "Feature",
+      id: "node/560446888",
+      properties: {
+        "@id": "node/560446888",
+        amenity: "Sampler",
+        name: "Sampler 7",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [-107.8846, 39.53912],
+      },
+    },
+    {
+      type: "Feature",
+      id: "node/574326909",
+      properties: {
+        "@id": "node/574326909",
+        amenity: "Quartz",
+        name: "Sampler 3",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [-107.88583, 39.53907],
+      },
+    },
+    {
+      type: "Feature",
+      id: "node/3607603463",
+      properties: {
+        "@id": "node/3607603463",
+        "addr:city": "Roma",
+        "addr:housenumber": "22",
+        "addr:postcode": "00185",
+        "addr:street": "Via Tiburtina",
+        amenity: "Sampler",
+        name: "Sampler 4",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [-107.88586, 39.53996],
+      },
+    },
+  ],
+};
---
+++ data/searchareas.geojson.js
@@ -0,0 +1,58 @@
+var searcharea = {
+  type: "FeatureCollection",
+  generator: "overpass-turbo",
+  copyright:
+    "The data included in this document is from www.openstreetmap.org. The data is made available under ODbL.",
+  timestamp: "2015-08-08T19:03:02Z",
+  features: [
+    {
+      type: "Feature",
+      properties: {
+        popupContent: "True",
+        name: "Garfield County Landfill",
+        address: "0075 Co Rd 246, Rifle, CO 81650",
+        date_open: "1/1/2022",
+        date_shut_down: "N/A",
+        amenity: "Landfill",
+      },
+      geometry: {
+        type: "Polygon",
+        coordinates: [
+          [
+            [-107.90307, 39.52223],
+            [-107.90301, 39.514632],
+            [-107.907194, 39.51441],
+            [-107.907151, 39.520624],
+            [-107.9053, 39.5221],
+            [-107.90307, 39.52223],
+          ],
+        ],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        popupContent: "True",
+        name: "South Canyon Landfill",
+        address: "1205 Co Hwy 134, Glenwood Springs, CO 81601",
+        date_open: "1/1/2022",
+        date_shut_down: "N/A",
+        amenity: "Landfill",
+      },
+      geometry: {
+        type: "Polygon",
+        coordinates: [
+          [
+            [-107.42477, 39.551],
+            [-107.42537, 39.55499],
+            [-107.42867, 39.55752],
+            [-107.43315, 39.55559],
+            [-107.43544, 39.55078],
+            [-107.43079, 39.55061],
+            [-107.42477, 39.551],
+          ],
+        ],
+      },
+    },
+  ],
+};
