6aa195c8a23549ae6328ac725abda8618181bb42
mineralstalker <104847447+mineralstalker@users.noreply.github.com>
2022-06-30 11:54:50 -0600

    Add files via upload

---
+++ Lithology Descriptor/Files/Lithologies.js
@@ -0,0 +1,27 @@
+var sandstone = {
+  name: "sandstone",
+  abbreviation: "SS:",
+  type: "clastic",
+  image: "",
+};
+
+var shale = {
+  name: "shale",
+  abbreviation: "SH:",
+  type: "clastic",
+  image: "",
+};
+
+var siltstone = {
+  name: "siltstone",
+  abbreviation: "SLTST:",
+  type: "clastic",
+  image: "",
+};
+
+var limestone = {
+  name: "limestone",
+  abbreviation: "LS",
+  type: "Evaporite",
+  image: "",
+};
